72888034fd0080d367683fed94c7fd5257deaf07
semenov <semenov>
2026-05-24 16:33:52 +0400

    hades отладка

--- src/main/java/org/hades/Hades.java
+++ src/main/java/org/hades/Hades.java
@@ -1,5 +1,6 @@
 package org.hades;
 
+import org.example.FilesService;
 import org.example.HttpClientService;
 import org.example.ParsingHtmlService;
 import org.jetbrains.annotations.NotNull;
@@ -11,21 +12,39 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Hades {
-    public static void main(String[] args) throws IOException, InterruptedException {
-//         Получение html  с  списком богов
-        String urlDarBog = "https://hades.fandom.com/ru/wiki/%D0%94%D0%B0%D1%80_%D0%B1%D0%BE%D0%B3%D0%BE%D0%B2";
-        HttpClientService httpClientService = new HttpClientService();
-        String html = httpClientService.getHttpResponseString(urlDarBog, "GET", null, null);
-        System.out.println(html);
-//        Парсинг html
-        ParsingHtmlService parsingHtmlService = new ParsingHtmlService();
-        Document document = parsingHtmlService.getDocument(html, "https://hades.fandom.com");
+
+    String domain = "https://hades.fandom.com";
+    String urlDarBog = "https://hades.fandom.com/ru/wiki/%D0%94%D0%B0%D1%80_%D0%B1%D0%BE%D0%B3%D0%BE%D0%B2";
+
+    String catalogHtmlHades = "E:\\CoreMain\\CoreMain\\src\\main\\resources\\hades\\";
+    String htmlNameFileHadesDarBog = "barBog.html";
+
+    HttpClientService httpClientService = new HttpClientService();
+    FilesService filesService = new FilesService();
+    ParsingHtmlService parsingHtmlService = new ParsingHtmlService();
+
+    void main(String[] args) throws IOException, InterruptedException {
+        // Получение html  с  списком богов
+        String html;
+
+        if(filesService.checkFile(catalogHtmlHades + htmlNameFileHadesDarBog)) {
+            System.out.println("Чтения с файла");
+            html = filesService.getFileString(catalogHtmlHades + htmlNameFileHadesDarBog);
+        } else {
+            System.out.println("Взята с сайта");
+            html = httpClientService.getHttpResponseString(urlDarBog, "GET", null, null);
+            filesService.createFile(catalogHtmlHades, htmlNameFileHadesDarBog, html);
+        }
+        parsingHtmlDarBogName(html);
+    }
+
+    public void parsingHtmlDarBogName(String html) {
+        Document document = parsingHtmlService.getDocument(html, domain);
         Nodes<@NotNull Element> itemBog = parsingHtmlService.getElements(document, "span[typeof='mw:File']");
         itemBog.forEach(element -> {
-            String urlBog = parsingHtmlService.getAttr(
-                    parsingHtmlService.getElement(element, "a.mw-file-description.image"),
-                    "href"
-            );
+            Element link = element.nextElementSibling();
+            assert link != null;
+            String urlBog = parsingHtmlService.getAttr(link, "href");
 
             Element imageElement = parsingHtmlService.getElement(element, "img.mw-file-element");
             String imageUrl = parsingHtmlService.getAttr(imageElement, "src");
@@ -33,10 +52,10 @@ public class Hades {
                 imageUrl = parsingHtmlService.getAttr(imageElement, "data-src");
             }
 
-            String name = parsingHtmlService.getText(Objects.requireNonNull(element.nextElementSibling()));
+            String name = parsingHtmlService.getText(Objects.requireNonNull(link));
 
             System.out.println("--------------------------");
-            System.out.println(urlBog);
+            System.out.println(domain + urlBog);
             System.out.println(imageUrl);
             System.out.println(name);
         });
